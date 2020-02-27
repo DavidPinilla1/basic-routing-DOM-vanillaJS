2194a21ea27090d50ec0822a10f4ebaa89296a5e
DavidPinilla92 <d.pinilla@geekshubs.com>
2020-02-27 12:02:52 +0100

    added amorfing scrolling and also some crazy progress bar according to the

--- script.js
+++ script.js
@@ -5,37 +5,54 @@ const Home = `
             </p>
             </div>
 `
-const Portfolio =`<div class="portfolio">
+const Portfolio = `<div class="portfolio">
                     <h2> Portfolio </h2>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil aperiam dolor ea velit repudiandae libero unde nisi numquam explicabo possimus sit odit architecto minus neque, ullam quidem deserunt adipisci impedit facilis delectus aspernatur? Minima corrupti a dignissimos reiciendis nulla magni praesentium ducimus placeat sed, distinctio, obcaecati consequatur doloribus rem nihil labore dolore! Placeat dolorum tempora deleniti? Voluptatibus cupiditate ab consectetur tempore voluptates aliquam, eius neque sunt vero quod. Magni maxime maiores consequuntur iusto minima quis voluptates amet sequi iste ex..
                     </p>
                 </div>`
 
-const Contacto =`<div class="contacto">
+const Contacto = `<div class="contacto">
                     <h2> Contacto </h2>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi perspiciatis adipisci accusantium fuga corrupti vitae iste, deleniti odit perferendis rem..
                     </p>
                 </div>`;
 const main = document.querySelector('main')
-window.addEventListener('load',()=>{
-    if(window.location.hash==='#'){
-        main.innerHTML =Home
-    }else  if(window.location.hash==='#portfolio'){
-        main.innerHTML =Portfolio
-    }else  if(window.location.hash==='#contacto'){
-        main.innerHTML =Contacto
-
+window.addEventListener('load', () => {
+    if (window.location.hash === '#portfolio') {
+        main.innerHTML = Portfolio
+    } else if (window.location.hash === '#contacto') {
+        main.innerHTML = Contacto
+    } else {
+        main.innerHTML = Home
     }
 })
-document.querySelector('.HomeButton').addEventListener('click',()=>{
+let widthProgressBar = 0;
+document.querySelector('.HomeButton').addEventListener('click', () => {
     main.innerHTML = Home;
-    history.pushState({},'','#');
+    window.scrollTo({
+        top: 400,
+        behavior: 'smooth',
+      })
+      setTimeout(() => {
+          document.body.scrollIntoView({ behavior: 'smooth' })
+      }, 1000);
+    document.querySelector('.progress-bar').style.width = '33.33%'
+    history.pushState({}, '', '#');
 })
-document.querySelector('.PortfolioButton').addEventListener('click',()=>{
+document.querySelector('.PortfolioButton').addEventListener('click', () => {
     main.innerHTML = Portfolio;
-    history.pushState({},'','#portfolio');
+    document.querySelector('.progress-bar').style.width = '66.66%'
+    history.pushState({}, '', '#portfolio');
 })
-document.querySelector('.ContactoButton').addEventListener('click',()=>{
+document.querySelector('.ContactoButton').addEventListener('click', () => {
     main.innerHTML = Contacto;
-    history.pushState({},'','#contacto');
+    document.querySelector('.progress-bar').style.width = '100%'
+    history.pushState({}, '', '#contacto');
 })
+// setInterval(() => {
+//     widthProgressBar++
+//     document.querySelector('.progress-bar').style.width = widthProgressBar + '%'
+//     if (widthProgressBar === 100) {
+//         widthProgressBar = 0;
+//     }
+// }, 200);
